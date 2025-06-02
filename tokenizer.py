b1e7af380ea2c8f5886d933470d8d5b040c24776
Mustapha BARKI <code4yo@gmail.com>
2025-06-02 07:45:20 +0200

    Update tokenizer.py
    Signed-off-by: Mustapha BARKI <code4yo@gmail.com>

--- tokenizer.py
+++ tokenizer.py
@@ -3,13 +3,13 @@ from tokenizers.models import BPE
 from tokenizers.trainers import BpeTrainer
 from tokenizers.pre_tokenizers import Whitespace
 
-# Initialiser le tokenizer
+# Initialize the tokenizer with BPE model
 tokenizer = Tokenizer(BPE())
 tokenizer.pre_tokenizer = Whitespace()
 
-# Entraîner le tokenizer
+# Train the tokenizer with special tokens
 trainer = BpeTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
 tokenizer.train(files=["path/to/your/dataset.txt"], trainer=trainer)
 
-# Sauvegarder le tokenizer
+# Save the trained tokenizer
 tokenizer.save("tokenizer.json")
